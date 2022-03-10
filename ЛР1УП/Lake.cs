83f91a33bedd3fac079a43316c6e6d639c93793f
Dasha <77586663+dasshabadyll@users.noreply.github.com>
2022-03-10 15:51:21 +0300

    Add files via upload

---
+++ ЛР1УП/Lake.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba1
+{
+    class Lake
+    {
+        public string Name;
+        public double Area;
+        public double Length;
+        public void SetName(string Name)
+        {
+            this.Name = Name;
+        }
+        public void SetArea(double Area)
+        {
+            this.Area = Area;
+        }
+        public void SetLength(double Length)
+        {
+            this.Length = Length;
+        }
+        public string GetName()
+        {
+            return this.Name;
+        }
+        public double GetArea()
+        {
+            return this.Area;
+        }
+        public double GetLength()
+        {
+            return this.Length;
+        }
+        public Lake(string Name, double Area, double Length)
+        {
+            this.Name = Name;
+            this.Area = Area;
+            this.Length = Length;
+        }
+        public Lake()
+        { }
+    }
+}
---
+++ ЛР1УП/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                int menu = 0;
+                List<Lake> counts = new List<Lake>();
+                do
+                {
+                    Console.WriteLine("1. Добавить реку");
+                    Console.WriteLine("2. Просмотреть реку");
+                    menu = Convert.ToInt32(Console.ReadLine());
+                    switch (menu)
+                    {
+                        case 1:
+                            Console.WriteLine("Введите название реки:");
+                            string name = Console.ReadLine();
+                            Console.WriteLine("Введите площадь бассейна:");
+                            double area = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine("Введите протяженность:");
+                            double length = Convert.ToDouble(Console.ReadLine());
+                            counts.Add(new Lake(name, area, length));
+                            break;
+                        case 2:
+                            Console.Write("Река");
+                            for (int i = 0; i < counts.Count; i++)
+                            {
+                                Console.Write(" " + counts[i].Name);
+                            }
+                            Console.WriteLine();
+                            Console.Write("Площадь бассейна");
+                            for (int i = 0; i < counts.Count; i++)
+                            {
+                                Console.Write(" " + counts[i].Area);
+                            }
+                            Console.WriteLine();
+                            Console.Write("Протяженность");
+                            for (int i = 0; i < counts.Count; i++)
+                            {
+                                Console.Write(" " + counts[i].Length);
+                            }
+                            Console.ReadKey();
+                            break;
+                    }
+                } while (menu < 3);
+                Console.ReadKey();
+            }
+            catch { }
+        }
+    }
+}
